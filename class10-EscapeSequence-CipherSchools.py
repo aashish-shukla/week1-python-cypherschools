5cfa1ede24453f58c0def8a01317f68171250e28
aashish_shukla <118830317+aashish-shukla@users.noreply.github.com>
2022-12-01 23:53:29 +0530

    Add files via upload

---
+++ class10-EscapeSequence-CipherSchools.py
@@ -0,0 +1,12 @@
+print("hello \"world\" world")
+print('i\'m aashish')
+#for new line:
+print("line A \nline B \n line C")
+#for tab "\t" is used
+print("name\tAashish")
+#for single backslash :
+print("this is backslash\\")
+#for double backslash:
+print("this is double backslash\\\\")
+#for backspace:
+print("hell\blo")
---
+++ class30-StepArguement-CipherSchools.py
@@ -0,0 +1,9 @@
+#slicing /selecting subsequences
+lang = "python"
+#syntax - [start arguement: stop arguement:step]
+print("Aashish"[2:5])     #output= shi
+print("Aashish"[2:5:1])   #output= shi
+print("Aashish"[2:5:2])   #output= si  
+print("Aashish"[0::2])    #output= asih
+print("Aashish"[8::-1])   #output = hsihsaA
+print("Aashish"[::-1])    #output = hsihsaA(TRICK)
